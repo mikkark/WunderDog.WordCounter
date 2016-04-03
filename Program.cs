a6ba3a8d79d3033452cadf17dea6106157d4fd8d
mikkark <mikkark@gmail.com>
2016-04-03 14:39:53 +0300

    Benchmark performance using BenchmarkDotNet

--- Program.cs
+++ Program.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace WunderDog.WordFinder
 {
     internal class Program
     {
         private static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<Benchmarker>();
+
+            //Console.ReadKey();
+
+            //return;
+
+            Stopwatch w = new Stopwatch();
+            w.Start();
+
+            var finder = Run();
+
+            w.Stop();
+
+            Console.WriteLine("finished in " + w.ElapsedMilliseconds + "ms.");
+            Console.WriteLine(finder.FoundWords.Count);
+            Console.WriteLine(finder.FoundWordsCount);
+
+            Console.ReadKey();
+        }
+
+        internal static WordFinder Run()
         {
             string plane0 = "AJFEAPUWOGMRMNXK";
             string plane1 = "DNSIFODSJEGIWKPR";
@@ -25,23 +49,18 @@ namespace WunderDog.WordFinder
                 plane3.ToCharArray().ToList()
             });
 
-            Stopwatch w = new Stopwatch();
-            w.Start();
-
             finder.FindWords();
 
-            w.Stop();
+            return finder;
+        }
+    }
 
-            Console.WriteLine("finished in " + w.ElapsedMilliseconds + "ms.");
-
-            Console.WriteLine(finder.FoundWordsCount);
-
-            //foreach (var found in finder.FoundWords)
-            //{
-            //    Console.WriteLine(found);
-            //}
-
-            Console.ReadKey();
+    public class Benchmarker
+    {
+        [Benchmark]
+        public void RunFullTestWithBenchmark()
+        {
+            Program.Run();
         }
     }
 }
